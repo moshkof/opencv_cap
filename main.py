6454d52bcd052f3b7e22fd54dc4c1b9e1bb3dba9
cataclysm78 <cataclysm78@yandex.ru>
2021-10-20 17:54:15 +0300

    Изменения на работе 20.10

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ while True:
     check, frame = video_capture.read()
 
     gray_video = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray_video = cv2.GaussianBlur(gray_video, (21, 21), 0)
+    gray_video = cv2.GaussianBlur(gray_video, (15, 15), 3)
 
     if first_frame is None:
         first_frame = gray_video
         continue
 
     delta_frame = cv2.absdiff(first_frame, gray_video)
-    thresh_frame = cv2.threshold(delta_frame, 100, 100, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(delta_frame, 100, 210, cv2.THRESH_BINARY)[1]
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
     (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 700:  # устанавливаем минимальный размер контуров
             continue
-        (x, y, w, h) = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x + w, x + h), (0, 255, 0), 3)
+        (x, y, w, h) = cv2.boundingRect(contour)  # задаем размеры обводки
+        cv2.rectangle(frame, ((x - 20), y), (x + 200, x - h), (0, 255, 255), 3) 
 
     cv2.imshow('Blur frame', gray_video)
     cv2.imshow("Delta frame", delta_frame)
